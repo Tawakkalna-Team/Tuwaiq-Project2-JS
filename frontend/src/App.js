9be96dc2209fe2938e1637b86b20bc1edea0dcbe
NadaAlraghieb <Nada.Alraghieb.eam.js@tuwaiq.edu.sa>
2021-11-21 17:45:22 +0300

    frontend for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Login from './components/main/Login'
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div className="App">	<Login/>
+	</div>;
 };
 
 export default App;
